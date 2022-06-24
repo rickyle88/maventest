d5eba5bb79e7ba1e21256ec8f591c7fdbe411a8c
Nguyen Le <bsbnguyen@gmail.com>
2022-06-23 19:40:30 -0700

    First

---
+++ src/test/java/qaclickacademy/mavenjava/AppiumTest.java
@@ -0,0 +1,15 @@
+package qaclickacademy.mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAppAndroid() {
+		System.out.println("NativeAppAndroid");
+	}
+	
+	@Test
+	public void iosApp() {
+		System.out.println("iosApp");
+	}
+}
---
+++ src/test/java/qaclickacademy/mavenjava/SeleniumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void BrowserAutomation() {
+		
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elements() {
+		System.out.println("elements");
+	}
+}
